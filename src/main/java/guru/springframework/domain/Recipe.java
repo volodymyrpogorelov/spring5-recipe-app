2f51834cdb357b01c24afbfd2ed8a46aefd03f5b
volodymyr.pogorelov <Volodymyr.Pogorelov@gmail.com>
2017-12-03 17:53:37 +0200

    RecipeRepository is added

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,8 +17,7 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //TODO: extend after type is defined
-    //private Difficulty difficulty;
+
     @Lob
     private Byte[] image;
 
@@ -30,6 +29,11 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -117,5 +121,21 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
