38a75c16e35e0135876538a4a7f355d78a4d9151
volodymyr.pogorelov <Volodymyr.Pogorelov@gmail.com>
2017-12-04 15:03:18 +0200

    More repositories are added and Index controller is added.

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,28 @@
+package guru.springframework.controllers;
+
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+
+        categoryRepository.findByDescription("American");
+        unitOfMeasureRepository.findByUom("Teaspoon");
+
+        return "index";
+
+    }
+}
